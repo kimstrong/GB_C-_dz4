326c2d808a8e8868c13aebc04babab0f40419918
kimst <kimstrong@mail.ru>
2023-02-02 17:45:20 +1100

    DZ4

---
+++ Sem_4_dz1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = number1;
+int i = 1;
+while (i<number2) {
+    result*=number1;
+    i++;
+}
+Console.WriteLine(result);
---
+++ Sem_4_dz3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+int[] array = GetBinaryArray(8);
+Console.WriteLine($"[{String.Join("; ", array)}]");
+int [] GetBinaryArray(int size)
+{
+    int[] resultArray = new int[size];
+    for (int i = 0;  i< size; i++)
+    {
+resultArray[i] = new Random().Next(100);
+    }
+    return resultArray;
+}
+
